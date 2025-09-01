bc87dcae69cbcbbb1d4da2e5aec477ec2b483e70
Bernhard <B.Jaeger1@gmx.net>
2025-09-01 14:20:21 +0200

    Fix batch size

--- team_code/slurm_train.sh
+++ team_code/slurm_train.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --time=3-00:00
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:4
 #SBATCH --cpus-per-task=32
 #SBATCH --output=/path/to/slurmlogs/%j.out  # File to which STDOUT will be written
 #SBATCH --error=/path/to/slurmlogs/%j.err   # File to which STDERR will be written
@@ -22,4 +22,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/path/to/miniconda/lib
 
 export OMP_NUM_THREADS=32  # Limits pytorch to spawn at most num cpus cores threads
 export OPENBLAS_NUM_THREADS=1  # Shuts off numpy multithreading, to avoid threads spawning other threads.
-torchrun --nnodes=1 --nproc_per_node=8 --max_restarts=0 --rdzv_id=$SLURM_JOB_ID --rdzv_backend=c10d train.py --id train_id_000 --batch_size 8 --setting 02_05_withheld --root_dir /path/to/dataset --logdir /path/to/logdir --use_controller_input_prediction 1 --use_wp_gru 0 --use_discrete_command 1 --use_tp 1 --continue_epoch 1 --cpu_cores 32 --num_repetitions 3
+torchrun --nnodes=1 --nproc_per_node=4 --max_restarts=0 --rdzv_id=$SLURM_JOB_ID --rdzv_backend=c10d train.py --id train_id_000 --batch_size 32 --setting 02_05_withheld --root_dir /path/to/dataset --logdir /path/to/logdir --use_controller_input_prediction 1 --use_wp_gru 0 --use_discrete_command 1 --use_tp 1 --continue_epoch 1 --cpu_cores 32 --num_repetitions 3
